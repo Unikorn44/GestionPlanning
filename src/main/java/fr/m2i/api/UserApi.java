8b2e4dd568c3e73875bb818ef17638ee1b828403
Charlotte Marion <marion.charlotte@orange.fr>
2022-08-25 09:28:54 +0200

    [Ajout] cors

--- src/main/java/fr/m2i/api/UserApi.java
+++ src/main/java/fr/m2i/api/UserApi.java
@@ -3,15 +3,20 @@ package fr.m2i.api;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import fr.m2i.models.User;
 
@@ -20,39 +25,24 @@ public class UserApi {
 	EntityManagerFactory factory;
 	EntityManager em;
 	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<User> getAll() {
-		
-		factory = Persistence.createEntityManagerFactory("UnityPersist");
-		em = factory.createEntityManager();
-		
-		List<User> users = em.createNamedQuery("selectAllUsers", User.class).getResultList();
-		
-		em.close();
-		factory.close();
-		
-		return users;
-	}
-	
 	// Récupération des informations User pour un user
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<User> findUserById(@PathParam("id") int id){
+	public User findUserById(@PathParam("id") int id){
 		
 		factory = Persistence.createEntityManagerFactory("UnityPersist");
 		em = factory.createEntityManager();
 		
 		Query q = em.createNamedQuery("selectUserById", User.class);
 		q.setParameter("id", id);
-		@SuppressWarnings("unchecked")
-		List<User> user = q.getResultList();
+		User user = (User) q.getSingleResult();
 		
 		em.close();
 		factory.close();
 		
 		return user;
+		
 	}
 	
 	// Récupération des informations User pour les contacts d'un user
@@ -73,5 +63,20 @@ public class UserApi {
 		
 		return users;
 	}
+	
+	// UPDATE informations user
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void updateUser(@FormParam("id") int id, @FormParam("first_name") String first_name,
+			@FormParam("last_name") String last_name, @FormParam("city") String city,
+			@FormParam("birthday_date") Date birthday_date, @FormParam("phone_number") String phone_number,
+			@FormParam("email") String email, @FormParam("admin") Boolean admin,
+			@FormParam("picture") String picture) {
 		
+		factory = Persistence.createEntityManagerFactory("UnityPersist");
+		em = factory.createEntityManager();
+		
+	}
+	
 }
