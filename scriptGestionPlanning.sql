9e3bed603a2afac676e7859b282ecaa63a355768
JeremyLao <jerem.0697@gmail.com>
2022-09-07 15:54:46 +0200

    [Ajout] d'un utilisateur jerem

--- scriptGestionPlanning.sql
+++ scriptGestionPlanning.sql
@@ -92,7 +92,8 @@ INSERT INTO planningTable (export, acces, modification)
  VALUES
  (true, true, true),
  (true, false, false),
- (false, false, false);
+ (false, false, false),
+ (true, true, true);
  
 -- création de 3 users dont 2 admins
 DELETE FROM userTable;
@@ -100,20 +101,23 @@ INSERT INTO userTable (first_name, last_name, city, birthday_date, phone_number,
  VALUES
  ('Toto', 'TEST', 'Lyon', '1900-05-10', '07 26 37 82 98', 'test@m2ifomration.fr', true, 1, 1 ),
  ('Tata', 'TEST2', 'Paris', '1965-12-21', '06 37 65 89 01', 'test2@m2ifomration.fr', false, 2, 1 ),
- ('Titi', 'TEST3', 'Nantes', '2012-12-03', '06 67 24 31 89', 'test3@m2ifomration.fr', true, 3 , 0 );
+ ('Titi', 'TEST3', 'Nantes', '2012-12-03', '06 67 24 31 89', 'test3@m2ifomration.fr', true, 3 , 0 ),
+ ('LAO', 'jeremy', 'Lyon', '1997-06-15', '11 22 33 44 55', 'jerem@m2ifomration.fr', true, 4 , 1 );
  
 INSERT INTO loginTable (login, password, id_user)
 VALUES
 	('test', '123456', 1 ),
 	('test2', '123456', 2 ),
-	('test3', '123456', 3 );
+	('test3', '123456', 3 ),
+    ('jerem', '7777', 4 );
 
 -- création de 2 relations dans la table list_user
 INSERT INTO List_userTable (id_user)
  VALUES
  (1),
  (2),
- (3);
+ (3),
+ (4);
  
  -- création de 2 relations dans la table list_user
 INSERT INTO ContactTable (id_user, id_list_user)
@@ -129,7 +133,10 @@ INSERT INTO eventTable (title, date_event, start_time, end_time, description)
  VALUES
  ('Présentation fil rouge', '2022-09-09', '09:00:00', '17:00:00', 'Présentation projet fil rouge'),
  ('Vacances', '2022-08-23', '08:30:00', '09:00:00', 'Fin des vacances'),
- ('Finish', '2022-09-15', '16:30:00', '17:00:00', 'Fin de formation');
+ ('Finish', '2022-10-15', '16:30:00', '17:00:00', 'Fin de formation'),
+ ('Spring Security', '2022-09-06', '8:30:00', '17:00:00', 'Apprentissage de Spring Security'),
+ ('Présentation blanc', '2022-09-07', '8:30:00', '17:00:00', 'On montre a Freddy'),
+ ('Présentation Cap Gemini', '2022-09-09', '15:00:00', '17:00:00', 'On montre a Cap G');
  
 -- création de 9 planning event
 INSERT INTO planning_eventTable (id_event, id_planning)
@@ -142,5 +149,9 @@ INSERT INTO planning_eventTable (id_event, id_planning)
  (2, 3),
  (3, 1),
  (3, 2),
- (3, 3);
+ (3, 3),
+ (3, 4),
+ (4, 4),
+ (5, 4),
+ (6, 4);
  
