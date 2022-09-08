36d227dc8f2b57db5a5ca1cc0de0b3ad48b7f488
Charlotte Marion <marion.charlotte@orange.fr>
2022-09-08 08:19:49 +0200

    [Modification] script sql

--- src/main/java/fr/m2i/api/UserApi.java
+++ src/main/java/fr/m2i/api/UserApi.java
@@ -11,7 +11,6 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
