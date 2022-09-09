d778a2934c44e39ae5fcf5fefb02fd2026cce238
PHDupont <phmel@orange.fr>
2022-09-09 09:32:36 +0200

    création test

--- src/test/java/fr/m2i/service/KeySingletonService.java
+++ src/test/java/fr/m2i/service/KeySingletonService.java
@@ -5,7 +5,7 @@ import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 
-public final class KeySingletonService {
+public class KeySingletonService {
 	
 	private static ECKey key;
 	
@@ -13,12 +13,12 @@ public final class KeySingletonService {
 	}
 	
 	// Création key
-    public static ECKey getKey() {
+    public ECKey getKey() {
         if (key == null) {
             try {
             	key = new ECKeyGenerator(Curve.P_256)
-				.keyID("1")
-				.generate();
+							.keyID("1")
+							.generate();
 			} catch (JOSEException e) {
 				e.printStackTrace();
 			}
--- src/test/java/fr/m2i/test/KeySingletonServiceTest.java
+++ src/test/java/fr/m2i/test/KeySingletonServiceTest.java
@@ -1,6 +1,8 @@
 package fr.m2i.test;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,31 +10,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.ECKey;
 
 import fr.m2i.service.KeySingletonService;
 
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 @ExtendWith(MockitoExtension.class)
 public class KeySingletonServiceTest {
 
-	@Mock
-	KeySingletonService kss;
 	
-	@BeforeEach
-	public void init() {
-		kss = new KeySingletonService();
-	}
 	
-	@SuppressWarnings("static-access")
+	
 	@Test
-	public void ServiceKeyTest() {
-		kss.getKey();
-		when(kss.getKey()).thenReturn(null);
-		kss.getKey();
-		final ECKey keyGenerated = kss.getKey();
-		verify(kss).getKey();
+	public void ServiceKeyTest() throws JOSEException {
+		
+		//ECKeyGenerator kgb = Mockito.mock(ECKeyGenerator.class, Mockito.withSettings().useConstructor(Curve.P_256));
+		
+		//ECKey anyKey = null;
+		//when(kgb.keyID("1").generate()).thenReturn(any(ECKey.class));
+		
+		KeySingletonService kiss = new KeySingletonService();
+		
+		final ECKey keyGenerated = kiss.getKey();
+		//verify(kgb).keyID("1").generate();
+		
+		assertThat(keyGenerated).isExactlyInstanceOf(ECKey.class);
 		
 	}
 	
